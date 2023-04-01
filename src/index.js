ee6d8a12b3116f2c38c9b6be6077d9ffca333994
Hugo <hugooloya@gmail.com>
2023-03-31 22:28:00 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
   // <BrowserRouter>
-    // {/* <HashRouter> */}
+  <HashRouter>
     <App />
-  // </BrowserRouter>
+    {/* </BrowserRouter> */}
   </HashRouter>
   // </React.StrictMode>
 );
