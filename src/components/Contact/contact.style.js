0d9099c59a09cc7898c4dfce79b31ac446ca4b11
Hugo <hugooloya@gmail.com>
2023-03-31 23:02:59 -0600

    Update contact.style.js

--- src/components/Contact/contact.style.js
+++ src/components/Contact/contact.style.js
@@ -2,11 +2,13 @@ import styled from "styled-components";
 
 export const MainDiv = styled.div`
   width: 100%;
-  /* min-height: 100vh; */
   display: flex;
   justify-content: center;
-  background-color: #f5f5f5;
+
+  @media screen and (max-width: 576px) {
+  }
 `;
+
 export const SForm = styled.form`
   width: 50%;
   height: 600px;
@@ -18,56 +20,82 @@ export const SForm = styled.form`
   border-radius: 35px;
   margin-top: 100px;
   box-shadow: 0 10px 10px -5px rgba(0, 0, 0, 0.5);
+
+  @media screen and (max-width: 576px) {
+    width: 90%;
+    height: 500px;
+    margin-top: 50px;
+  }
 `;
+
 export const FormH1 = styled.h1`
   font-size: 45px;
   font-weight: 500;
-  font-family: "Roboto", sans-serif;
   text-align: center;
   margin-bottom: 50px;
   color: #f5f5f5;
+
+  @media screen and (max-width: 576px) {
+    font-size: 30px;
+    margin-bottom: 20px;
+  }
 `;
+
 export const MiniDiv = styled.div`
   width: 100%;
   display: flex;
+  justify-content: space-between;
+
+  @media screen and (max-width: 576px) {
+    flex-direction: column;
+  }
 `;
+
 export const SInput = styled.input`
-  width: 50%;
-  margin: 10px;
+  width: 48%;
   padding: 10px;
-  font-family: "Roboto", sans-serif;
   font-size: 20px;
   outline: none;
   border-radius: 10px;
   border: none;
+  margin-bottom: 25px;
+
+  @media screen and (max-width: 576px) {
+    width: 100%;
+  }
 `;
 
 export const STextArea = styled.textarea`
-  width: 98%;
-  margin: 10px;
+  width: 100%;
   padding: 10px;
-  font-family: "Roboto", sans-serif;
   font-size: 20px;
   outline: none;
   border-radius: 10px;
   border: none;
+
+  @media screen and (max-width: 576px) {
+  }
 `;
+
 export const SButton = styled.button`
   width: 150px;
   height: 50px;
   border-radius: 30px;
   border: 2px solid #324936;
-  font-family: "Roboto", sans-serif;
   font-weight: 700;
   font-size: 18px;
   color: #324936;
   cursor: pointer;
-  margin-top: 20px;
+  margin-top: 35px;
+  outline: none;
 
   &:hover {
-    /* transition: all 0.5s ease; */
     background-color: #324936;
     color: #f5f5f5;
     border: 2px solid #f5f5f5;
+  }
+
+  @media screen and (max-width: 576px) {
+    height: 120px;
   }
 `;
