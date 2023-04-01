b27e4471f07000e350649c54d2ccf154dd0f452c
Hugo <hugooloya@gmail.com>
2023-03-31 22:31:26 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import LandingPage from "./components/LandingPage/index";
+import LandingPage from "./components/LandingPage";
 import Navbar from "./components/Navbar";
 import Contact from "./components/Contact";
 import GlobalStyle from "./Global.style";
@@ -13,7 +13,7 @@ export default function App() {
       <Navbar />
       <Routes>
         <Route
-          path="/LandingPage"
+          path="/"
           element={<LandingPage Backend={Backend} Frontend={Frontend} />}
         />
         <Route path="/Contact" element={<Contact />} />
